e00d072b38dbafbdf7a76b908b487f6205c2aea7
NicolasGenta <nicolasgenta93@gmail.com>
2023-10-05 21:33:51 -0300

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 import Header from './componentes/Header';
 import Footer from './componentes/Footer';
 import Registro from './componentes/registro/Registro';
-import Home from './componentes/Home';
-import Tienda from './componentes/Tienda';
+//import Home from './componentes/Home';
+//import Tienda from './componentes/Tienda';
 import Contacto from './componentes/Contacto';
 
 function App() {
@@ -15,10 +15,10 @@ function App() {
     <Router>
      <Header/>
       <Swith>
-       <Router path = "/home" component = {Home}/>
-       <Router path = "/tienda" component = {Tienda}/>
-       <Router path = "/contacto" component = {Contacto}/>
-       <Router path = "/registro" component = {Registro}/>
+       {/* <Router path = "/home" component = {Home}/> */}
+       {/* <Router path = "/tienda" component = {Tienda}/> */}
+       <Router path = "/contacto" component = {<Contacto/>}/>
+       <Router path = "/registro" component = {<Registro/>}/>
       </Swith>
      <Footer/>
     </Router>
